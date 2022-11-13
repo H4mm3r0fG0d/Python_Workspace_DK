690758464062d8c3bb2943732d3c90863c60fabe
diegokendall <dak2017@email.vccs.edu>
2022-11-12 21:20:56 -0500

    Testing Authentication Login

--- Python Crash Course/helloworld.py
+++ Python Crash Course/helloworld.py
@@ -1,2 +1,7 @@
 msg = "Hello Python People! ARE YOU READY TO GET CODING?!?!"
 print(msg)
+
+msg2 = "This is a test of the system-\n"
+msg3 = "TESTING!---------------------\n"
+msg4 = "TESTING!---------------------"
+print(msg2 + msg3 + msg4)
